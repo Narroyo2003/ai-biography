8f3c8b038f073e63feac3d68bdbf3e5323ab20a8
Nicolas Arroyo <arroyonicolas125@gmail.com>
2026-09-12 00:06:30 -0400

    Allow frontend CORS

--- biography/biography/src/main/java/com/nicolas/biography/BiographyController.java
+++ biography/biography/src/main/java/com/nicolas/biography/BiographyController.java
@@ -1,10 +1,14 @@
 package com.nicolas.biography;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class BiographyController {
 
     private final OpenAIService openAIService;
